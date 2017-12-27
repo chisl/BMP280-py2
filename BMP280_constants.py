2ca9e1217883bbf3a643d88d22e6a378157de9e7
chisl <info@chisl.io>
2017-12-27 18:11:35 +0100

    Initial commit

---
+++ BMP280_constants.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""BMP280: The BMP280 is an absolute barometric pressure sensor especially designed for mobile applications."""
+
+__author__     = "ChISL"
+__copyright__  = "TBD"
+__credits__    = ["Bosch Sensortec"]
+__license__    = "TBD"
+__version__    = "0.1"
+__maintainer__ = "https://chisl.io"
+__email__      = "info@chisl.io"
+__status__     = "Test"
+
+class REG:
+	DEVID = 0
+	ID = 208
+	RESET = 224
+	STATUS = 243
+	CTRL_MEAS = 244
+	CONFIG = 245
+	PRESS = 247
+	TEMP = 250
+	CALIB = 136
